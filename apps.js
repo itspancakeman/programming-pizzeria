2b11ea06e193300d8bdfd98942c4c1871dce4e24
itspancakeman <collinjst.onge@gmail.com>
2024-04-13 15:55:52 -0400

    feat: created functions to spawn random customers after start screen disappears

--- apps.js
+++ apps.js
@@ -2,6 +2,7 @@
 const game = document.getElementById('game');
 const score = document.getElementById('score');
 const failedOrders = document.getElementById('failed-orders');
+const ctx = game.getContext('2d');
 let customer
 let player
 
@@ -16,25 +17,32 @@ window.addEventListener('DOMContentLoaded', function() {
             splashScreen.classList.add('hidden')
         },610)
         document.getElementById('game').style.display = '';
+        initCustomer();
+        const runGame = this.setInterval(gameLoop, 60);
     });
 });
 
 // =============== SETUP FOR CANVAS RENDERING ============
-
-
-
+game.width = 1200; 
+game.height = 600;
 
 // ============== ENTITIES ==================
 class Customer {
-    constructor(x, y, img, width, height) {
+    constructor(x, y, color, width, height) {
         this.x = x;
         this.y = y;
-        this.img = img;
+        this.color = color;
         this.width = width;
         this.height = height;
+        this.inside = true;
 
+        this.render = function() {
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
 }
+
 
 
 
@@ -44,10 +52,28 @@ class Customer {
 
 
 // =============== HELPER FUNCTIONS =================
+function initCustomer() {
+    setTimeout(function() {
+        let randomX = Math.floor(Math.random() * (game.width - 75));
+        let randomY = Math.floor(Math.random() * (game.height - 120));
+        const colors = ['#7BD3EA', 'A1EEBD', 'F6F7C4', 'F6D6D6'];
+        let randomIndex = Math.floor(Math.random() * (colors.length -1));
+        let randomColor = colors[randomIndex];
+        customer = new Customer(randomX, randomY, randomColor, 75, 120);
+    }, 500)
+    
+    return true;
+
+}
 
 
 
 
 
 // ================== GAME PROCESSES ===============
-
+function gameLoop() {
+    ctx.clearRect(0, 0, game.width, game.height);
+    if (customer.inside) {
+        customer.render();
+    }
+}
