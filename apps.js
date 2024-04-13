6da8d8c8ae8a80708ec3dc9c2b9f1c863a7372be
itspancakeman <collinjst.onge@gmail.com>
2024-04-13 13:42:50 -0400

    feat: added outline plan for javascript and established some DOM elements

---
+++ apps.js
@@ -0,0 +1,35 @@
+// GLOBAL DOM AND VARIABLES
+const game = document.getElementById('game');
+const score = document.getElementById('score');
+const failedOrders = document.getElementById('failed-orders');
+let customer
+let player
+
+
+
+// =============== PAINT INITIAL SCREEN ===============
+
+
+
+// =============== SETUP FOR CANVAS RENDERING ============
+
+
+
+
+// ============== ENTITIES ==================
+
+
+
+
+// ================= KEYBOARD LOGIC =================
+
+
+
+
+// =============== HELPER FUNCTIONS =================
+
+
+
+
+
+// ================== GAME PROCESSES ===============
